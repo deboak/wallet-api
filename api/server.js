4f3be1b90b67d5028d345d9edd2ea414a7b4f685
deboak <adebowaleakinode9@gmail.com>
2025-07-29 23:31:55 +0100

    Update: fixed README.MD, deleted api/ndex.js, modified app.js, modified package-lock.json, modified package.json, modified server.js, updated vercel.js

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const serverless = require('serverless-http');
+const app = require('../app');
+const connectDB = require('../db/connect');
+
+let isConnected = false;
+
+module.exports = async (req, res) => {
+  if (!isConnected) {
+    await connectDB(process.env.MONGO_URI);
+    isConnected = true;
+  }
+
+  const handler = serverless(app);
+  return handler(req, res);
+};
--- app.js
+++ app.js
@@ -45,19 +45,21 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
 
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}...`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+// const start = async () => {
+//     try {
+//         await connectDB(process.env.MONGO_URI);
+//         app.listen(port, () => {
+//             console.log(`Server is listening on port ${port}...`);
+//         })
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-start();
+// start();
+
+module.exports = app; 
 
 
