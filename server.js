30dcfe7c19bf67154cb1c8113eac0bdff7af5d31
deboak <adebowaleakinode9@gmail.com>
2025-07-30 12:25:27 +0100

    Updated: added api/server.js, modified server.js, modified vercel.json

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const app = require('./app');
-const connectDB = require('./db/connect');
-const port = process.env.PORT || 3001;
+// const app = require('./app');
+// const connectDB = require('./db/connect');
+// const port = process.env.PORT || 3001;
 
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        console.log('connected to MongoDB');
+// const start = async () => {
+//     try {
+//         await connectDB(process.env.MONGO_URI);
+//         console.log('connected to MongoDB');
         
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}...`);
-        })
-    } catch (error) {
-        console.log('failed to connect to MongoDB:', error);
-        process.exit(1); 
-    }
-}
+//         app.listen(port, () => {
+//             console.log(`Server is listening on port ${port}...`);
+//         })
+//     } catch (error) {
+//         console.log('failed to connect to MongoDB:', error);
+//         process.exit(1); 
+//     }
+// }
 
-start();
+// start();
