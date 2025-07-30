0e63a8c9f56767f28331d751a91bb864c3aafe36
deboak <adebowaleakinode9@gmail.com>
2025-07-30 23:10:14 +0100

    Updated: modified server.js, modified app.js

--- app.js
+++ app.js
@@ -51,21 +51,23 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
+const port = process.env.PORT || 3001;
 
-// const port = process.env.PORT || 3001;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log('connected to MongoDB');
+        
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log('failed to connect to MongoDB:', error);
+        process.exit(1); 
+    }
+}
 
-// const start = async () => {
-//     try {
-//         await connectDB(process.env.MONGO_URI);
-//         app.listen(port, () => {
-//             console.log(`Server is listening on port ${port}...`);
-//         })
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// start();
+start();
 
 module.exports = app; 
 
